d3df8c46da8206555e132bb0287d05f1ca55ef30
Manny Sangha <mannysangha@Mannys-MacBook-Pro.local>
2022-08-03 04:02:23 -0700

    finished

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,14 +1,34 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
-function renderLicenseBadge(license) {}
 
+const Choices = require("inquirer/lib/objects/choices");
+const { listenerCount, chdir } = require("process");
+
+// If there is no license, return an empty string
+function renderLicenseBadge(license) {
+  if(license !== 'None') {
+    return `[![GNU Public License](https://img.shields.io/badge/GNU-general%20public%20license-green)]`
+  } else {
+    return ``
+  }
+}
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  return `https://www.gnu.org/licenses/lgpl-3.0.en.html`;
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if (license !== 'None') {
+    return `
+    ## license
+
+    License is ${license} standard license`
+  } else {
+    return ``
+  }
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
