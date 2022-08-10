14758dfbaa25d285dbcc428736fe90b66365dd04
Manny Sangha <mannysangha@Mannys-MacBook-Pro.local>
2022-08-10 03:50:30 -0700

    fixed errors

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -31,13 +31,22 @@ function renderLicenseLink(license) {
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
 function renderLicenseSection(license) {
-  if (license !== 'None') {
+  if (license === 'MIT') {
     return `
     ## license
 
     License is ${license} standard license`
-  } else {
-    return ``
+  } else if (license === "MPL") {
+    return `
+     ## license
+
+    License is ${license} standard license`
+  }
+  else if(license === "GPL") {
+    return ` 
+    ## license
+
+    License is ${license} standard license`
   }
 }
 
@@ -76,7 +85,7 @@ ${renderLicenseBadge(data.licenses)}
 
 ${renderLicenseSection(data.licenses)}
 
-${renderLicenseLink(data.license)}
+${renderLicenseLink(data.licenses)}
 
  ## Contributing
 
